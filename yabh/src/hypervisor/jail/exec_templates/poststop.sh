71fa1551ba3ed3c9f33401429b9acc05af30ad49
Guillaume Pelure <onyxmsi@laposte.com>
2023-08-21 19:33:14 +0200

    jail: remove useless check on network interface on exec.poststop

--- yabh/src/hypervisor/jail/exec_templates/poststop.sh
+++ yabh/src/hypervisor/jail/exec_templates/poststop.sh
@@ -13,9 +13,6 @@ dbg "[$YABH_JAIL_NAME] Execute post stop script"
 # Delete interfaces
 for if_str in $(yabh_run jail interface list $YABH_JAIL_NAME) ; do
     if_name=$(csvline_get_field 1 "$if_str")
-    if ! inet_if_exists $if_name ; then
-        crt 1 "$if_name: no such interface"
-    fi
     dbg "[$YABH_JAIL_NAME] Destroy interface $if_name"
     ifconfig $if_name destroy || true
 done
