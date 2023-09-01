667ac9d421825ae935f16ec6c1bc2e5ef5a077ae
Guillaume Pelure <onyxmsi@laposte.com>
2023-09-01 09:15:44 +0200

    jail: fix relase script

--- yabh/src/hypervisor/jail/exec_templates/release.sh
+++ yabh/src/hypervisor/jail/exec_templates/release.sh
@@ -1,5 +1,19 @@
 set -e .
 
+delete_ifs() {
+    local idx=0
+    local if_prefix=$1
+    while : ; do
+        if_name="$if_prefix${idx}a"
+        if ! inet_if_exists $if_name ; then
+            break
+        fi
+        dbg "[$YABH_JAIL_NAME] Destroy interface $if_name"
+        ifconfig $if_name destroy || true
+        idx=$(($idx + 1))
+    done
+}
+
 SCRIPTNAME=$(basename $0)
 COMMON_PATH="$(dirname $0)/exec_common.sh"
 if [ ! -f $COMMON_PATH ] ; then
@@ -11,12 +25,5 @@ load_environment
 
 dbg "[$YABH_JAIL_NAME] Execute release script"
 # Delete interfaces
-idx=0
-while : ; do
-    if_name="${YABH_JAIL_NAME}.${idx}a"
-    if ! inet_if_exists $if_name ; then
-        break
-    fi
-    dbg "[$YABH_JAIL_NAME] Destroy interface $if_name"
-    ifconfig $if_name destroy || true
-done
+delete_ifs epair
+delete_ifs "${YABH_JAIL_NAME}."
